18823db008e1da0c97a13ab53be34f7ada093d34
Ronnie Casasola <ronniecasasola@gmail.com>
2018-04-21 22:33:38 -0700

    fixxxx

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Toolify</h1>
           <Login />
         </header>
